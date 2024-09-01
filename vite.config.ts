4ee2a4f0c4bf5394b583d67d18d902dbe6f2eeaa
Adrien Touzouli <contact@ad2ien.dev>
2024-09-01 15:40:57 +0200

    🐛 fix gh page deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ideal-wages-calculator-vue/',
   plugins: [
     vue()
     // vuetify({ autoImport: true })
